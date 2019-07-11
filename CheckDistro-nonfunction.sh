74b9ec754abb9a4b3b108fabd9379ed5926c4e92
Evan Bolt <boltevan7@gmail.com>
2019-07-11 18:54:52 -0400

    Update CheckDistro-nonfunction.sh

--- CheckDistro-nonfunction.sh
+++ CheckDistro-nonfunction.sh
@@ -3,26 +3,26 @@
 # /etc/os-release is the newer standard w/ systemd, look for that first
 if [ -f "/etc/os-release" ]
 then
-	echo "/etc/os-release found"
-	DISTRO=$(cat /etc/os-release | head -n 1 | cut -d "=" -f 2 | tr '[:upper:]' '[:lower:]' | tr -d '[:space:]' | tr -d '\"')
-
-# hostnamectl is still newer, but a nice fallback and exists on some systems not running systemd (ex: Ubuntu 14.04
-elif [ -x "$( command -v hostnamectl )" ]
+    	echo "/etc/os-release found"
+        DISTRO=$(cat /etc/os-release | head -n 1 | cut -d "=" -f 2 | tr '[:upper:]' '[:lower:]' | tr -d '\"')
+fi
+# hostnamectl is still newer, but a nice fallback and exists on some systems not running systemd (ex: Ubuntu 14.04)
+if [ -x "$( command -v hostnamectl )" ]
 then
-	echo "hostnamectl found"
-	DISTRO=$(hostnamectl | grep "Operating System" | cut -d ":" -f 2 | sed -e 's/^[[:space:]]*//' | cut -d " " -f 1 | tr '[:upper:]' '[:lower:]')
+    	echo "hostnamectl found"
+        DISTRO=$(hostnamectl | grep "Operating System" | cut -d ":" -f 2 | sed -e 's/^[[:space:]]*//' | tr '[:upper:]' '[:lower:]')
 
 fi
 
 if [[ ${DISTRO} == *"arch"* || ${DISTRO} == *"manjaro"* ]]
 then
-	echo "Distro is ArchLinux based"
-elif [[ ${DISTRO} == *"centos"* || ${DISTRO} == *"fedora"* || ${DISTRO} == *"redhat"* ]]
+    	echo "Distro is ArchLinux based"
+elif [[ ${DISTRO} == *"centos"* || ${DISTRO} == *"fedora"* || ${DISTRO} == *"red hat"* ]]
 then
     echo "Distro is RedHat based"
 elif [[ ${DISTRO} == *"debian"* || ${DISTRO} == *"ubuntu"* || ${DISTRO} == *"mint"* ]]
 then
-	echo "Distro is Debian-based"
+    	echo "Distro is Debian-based"
 fi
 
 echo "${DISTRO}"
