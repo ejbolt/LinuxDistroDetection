8a57356d83b7ace862dc65e5feadf7dac3abde6d
Evan Bolt <boltevan7@gmail.com>
2019-11-27 23:21:29 -0500

    Update CheckDistro-nonfunction.sh

--- CheckDistro-nonfunction.sh
+++ CheckDistro-nonfunction.sh
@@ -13,16 +13,30 @@ then
 	DISTRO=$(hostnamectl | grep "Operating System" | cut -d ":" -f 2 | sed -e 's/^[[:space:]]*//' | tr '[:upper:]' '[:lower:]')
 
 fi
-
-if [[ ${DISTRO} == *"arch"* || ${DISTRO} == *"manjaro"* ]]
-then
-	echo "Distro is ArchLinux based"
-elif [[ ${DISTRO} == *"centos"* || ${DISTRO} == *"fedora"* || ${DISTRO} == *"red hat"* ]]
-then
-    echo "Distro is RedHat based"
-elif [[ ${DISTRO} == *"debian"* || ${DISTRO} == *"ubuntu"* || ${DISTRO} == *"mint"* ]]
-then
-	echo "Distro is Debian-based"
-fi
+	if [[ ${DISTRO_CHECK} == *"arch"* ]]
+	then
+		DISTRO="arch"
+	elif [[ ${DISTRO_CHECK} == *"manjaro"* ]]
+	then
+	    echo "manjaro"
+	elif [[ ${DISTRO_CHECK} == *"centos"* ]]
+	then
+		DISTRO="centos"
+	elif [[ ${DISTRO_CHECK} == *"fedora"* ]]
+	then
+		DISTRO="fedora"
+	elif [[ ${DISTRO_CHECK} == *"rhel"* ]]
+	then
+	    DISTRO="rhel"
+	elif [[ ${DISTRO_CHECK} == *"debian"* ]]
+	then
+		DISTRO="debian"
+	elif [[ ${DISTRO_CHECK} == *"ubuntu"* ]]
+	then
+		DISTRO="ubuntu"
+	elif [[ ${DISTRO_CHECK} == *"mint"* ]]
+	then
+        DISTRO="mint"
+	fi
 
 echo "${DISTRO}"
