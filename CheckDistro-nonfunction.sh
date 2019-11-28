186d4ed34df54395b492373700c9aa910210c553
Evan Bolt <boltevan7@gmail.com>
2019-11-28 00:07:15 -0500

    Update CheckDistro-nonfunction.sh

--- CheckDistro-nonfunction.sh
+++ CheckDistro-nonfunction.sh
@@ -3,13 +3,11 @@
 # /etc/os-release is the newer standard w/ systemd, look for that first
 if [ -f "/etc/os-release" ]
 then
-	echo "/etc/os-release found"
 	DISTRO=$(cat /etc/os-release | head -n 1 | cut -d "=" -f 2 | tr '[:upper:]' '[:lower:]' | tr -d '\"')
 
 # hostnamectl is still newer, but a nice fallback and exists on some systems not running systemd (ex: Ubuntu 14.04)
 elif [ -x "$( command -v hostnamectl )" ]
 then
-	echo "hostnamectl found"
 	DISTRO=$(hostnamectl | grep "Operating System" | cut -d ":" -f 2 | sed -e 's/^[[:space:]]*//' | tr '[:upper:]' '[:lower:]')
 
 fi
